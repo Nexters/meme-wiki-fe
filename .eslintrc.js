10236abe76839cd171acab1ca29c1a6fde4822ef
Cllaude99 <xodbsrla1025@naver.com>
2025-07-15 16:08:52 +0900

    chore: .eslintrc.js 파일 수정
    - TypeScript 관련 규칙을 추가하여 함수 호출 및 반환 값 타입 체크 완화
    - Promise 관련 경고로 변경
    - 템플릿 리터럴 타입 체크 완화

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,10 @@ module.exports = {
     '@typescript-eslint/no-unsafe-member-access': 'off',
     '@typescript-eslint/no-unsafe-argument': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off', // 함수 호출 관련 타입 체크 완화
+    '@typescript-eslint/no-unsafe-return': 'off', // 반환 값 타입 체크 완화
+    '@typescript-eslint/no-floating-promises': 'warn', // Promise 관련 경고로 변경
+    '@typescript-eslint/restrict-template-expressions': 'off', // 템플릿 리터럴 타입 체크 완화
     'unused-imports/no-unused-imports': 'error',
     'unused-imports/no-unused-vars': [
       'warn',
