958c337ddf385f14a553ebbcf8194f4ef1105d9d
Cllaude99 <xodbsrla1025@naver.com>
2025-08-11 00:03:18 +0900

    chore: ESLint 및 TypeScript 설정 패키지 이름 변경
    - ESLint 및 TypeScript 설정의 패키지 이름을 '@cllaude99_lib'에서 '@meme_wiki'로 변경
    - 관련된 모든 설정 파일 및 패키지에서 이름을 일관되게 수정

--- packages/ui/.eslintrc.cjs
+++ packages/ui/.eslintrc.cjs
@@ -2,7 +2,7 @@
 module.exports = {
   root: true,
   ignorePatterns: ['.eslintrc.cjs'],
-  extends: ['@cllaude99_lib/eslint-config/index.js'],
+  extends: ['@meme_wiki/eslint-config/index.js'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: true,
